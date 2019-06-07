50fc65dac2e6fdb86f6011fcfe966b9ec9bec934
Cesar Zavala <zvala93@gmail.com>
2019-06-06 22:22:27 -0500

    taller

---
+++ Taller3/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Taller3.Controllers
+{
+    public class AuthorController : Controller
+    {
+        booksEntities db = new booksEntities();
+
+        public ActionResult Index(string searching)
+        {
+            var author = from s in db.authors select s;
+
+            if (!String.IsNullOrEmpty(searching)) {
+                author = author.Where(s => s.au_fname.Contains(searching));
+            }
+
+            return View(author.ToList());
+        }
+    }
+}
